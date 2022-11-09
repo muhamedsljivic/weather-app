e8341c16fe0a71f0991ac2ccfd975340cfbcac70
Muhamed Sljivic <muhamedica09@gmail.com>
2022-11-09 01:07:06 +0100

    Upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import classes from "./App.module.css";
+import Search from "./components/Search";
+import img from "./imgs/weather-1.jpg";
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
   return (
-    <>
-      <h1>React app</h1>
-    </>
+    <div className={classes.container}>
+      <img className={classes.img} src={img} alt="Weather" />
+      <div className={classes.weatherInfo}>
+        <Search onSearchChange={handleOnSearchChange} />
+      </div>
+    </div>
   );
 }
 
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c249ef30a3msh6616d843909cf6fp1bf4acjsn7b2f25f880dc",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
